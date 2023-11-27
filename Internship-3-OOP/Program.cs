fc0856488ed47bccec135f0e698b2e547a9dfbda
thecarbophile <hello@carbophile.com>
2023-11-27 22:27:41 +0100

    Create phonebook management system

--- Internship-3-OOP/Program.cs
+++ Internship-3-OOP/Program.cs
@@ -2,6 +2,7 @@
 
 var phoneBook = new Dictionary<Contact, List<Call>>();
 
+Console.WriteLine("Dobrodošli u telefosnki imenik! Da bi ste otkazali unos u bilo kojem trenutku, unesite Ctrl + Z!");
 while (true)
 {
     Console.WriteLine("1 - Ispis svih kontakata");
@@ -43,22 +44,165 @@ while (true)
 
 void PrintContacts()
 {
-    //TODO: Implement
+    foreach (var contact in phoneBook.Keys)
+        Console.WriteLine(
+            $"Ime: {contact.Name}, Broj mobitela: {contact.PhoneNumber}, Preferenca: {contact.Preference}");
 }
 
 void AddContacts()
 {
-    //TODO: Implement
+    string? name;
+    while (true)
+    {
+        Console.WriteLine("Unesite ime kontakta");
+        name = Console.ReadLine();
+
+        if (name == null) return;
+
+        if (string.IsNullOrEmpty(name))
+        {
+            Console.WriteLine("Ime ne može biti prazno!");
+            continue;
+        }
+
+        break;
+    }
+
+    string? phoneNumber;
+    while (true)
+    {
+        var breakLoop = false;
+        Console.WriteLine("Unesite broj mobitela kontakta");
+        phoneNumber = Console.ReadLine();
+
+        if (phoneNumber == null) return;
+
+        if (string.IsNullOrEmpty(phoneNumber))
+        {
+            Console.WriteLine("Broj mobitela ne može biti prazan!");
+            continue;
+        }
+
+        if (phoneNumber.Length != 13)
+        {
+            Console.WriteLine("Unesite broj mobitela u internacionalnom formatu (+385910000000)!");
+            continue;
+        }
+
+        var i = 0;
+        foreach (var digit in phoneNumber)
+        {
+            i++;
+            if (char.IsDigit(digit) || digit == '+' && i == 1) continue;
+            Console.WriteLine("Broj mobitela može sadržavati samo brojeve (s iznikom plusa na početuku)!");
+            breakLoop = true;
+            break;
+        }
+
+        if (breakLoop)
+            continue;
+
+        var number = phoneNumber;
+        if (phoneBook.Keys.Any(c => c.PhoneNumber == number))
+        {
+            Console.WriteLine("Broj mobitela već postoji!");
+            continue;
+        }
+
+        break;
+    }
+    
+    ContactPreference preference;
+    while (true)
+    {
+        Console.WriteLine("Unesite preferencu kontakta (1 - Favorit, 2 - Normalan, 3 - Blokiran)");
+        var preferenceInput = Console.ReadLine();
+
+        if (preferenceInput == null) return;
+
+        switch (preferenceInput)
+        {
+            case "1":
+                preference = ContactPreference.Favorite;
+                break;
+            case "2":
+                preference = ContactPreference.Regular;
+                break;
+            case "3":
+                preference = ContactPreference.Blocked;
+                break;
+            default:
+                Console.WriteLine("Krivi unos!");
+                continue;
+        }
+
+        break;
+    }
+    
+    phoneBook.Add(new Contact(name, phoneNumber, preference), new List<Call>());
 }
 
 void RemoveContact()
 {
-    //TODO: Implement
+    while (true)
+    {
+        Console.WriteLine("Unesite broj mobitela kontakta kojeg želite izbrisati:");
+        var phoneNumber = Console.ReadLine();
+        if (phoneNumber == null) return;
+        var contact = phoneBook.Keys.FirstOrDefault(c => c.PhoneNumber == phoneNumber);
+        if (contact == null)
+        {
+            Console.WriteLine("Ne postoji kontakt s unesenim brojem mobitela!");
+            continue;
+        }
+        phoneBook.Remove(contact);
+        break;
+    }
 }
 
 void EditContactPreference()
 {
-    //TODO: Implement
+    Contact? contact;
+    while (true)
+    {
+        Console.WriteLine("Unesite broj mobitela kontakta kojeg želite editirati:");
+        var phoneNumber = Console.ReadLine();
+        if (phoneNumber == null) return;
+        
+        contact = phoneBook.Keys.FirstOrDefault(c => c.PhoneNumber == phoneNumber);
+        if (contact == null)
+        {
+            Console.WriteLine("Ne postoji kontakt s unesenim brojem mobitela!");
+            continue;
+        }
+        break;
+    }
+    
+    while (true)
+    {
+        Console.WriteLine("Unesite novu preferencu kontakta (1 - Favorit, 2 - Normalan, 3 - Blokiran)");
+        var preferenceInput = Console.ReadLine();
+
+        if (preferenceInput == null) return;
+
+        switch (preferenceInput)
+        {
+            case "1":
+                contact.Preference = ContactPreference.Favorite;
+                break;
+            case "2":
+                contact.Preference = ContactPreference.Regular;
+                break;
+            case "3":
+                contact.Preference = ContactPreference.Blocked;
+                break;
+            default:
+                Console.WriteLine("Krivi unos!");
+                continue;
+        }
+
+        break;
+    }
 }
 
 void ManageContact()
@@ -67,12 +211,14 @@ void ManageContact()
     {
         Console.WriteLine("Unesite broj mobitela kontakta kojim želite upravljati:");
         var phoneNumber = Console.ReadLine();
+        if (phoneNumber == null) return;
         var contact = phoneBook.Keys.FirstOrDefault(c => c.PhoneNumber == phoneNumber);
         if (contact == null)
         {
             Console.WriteLine("Ne postoji kontakt s unesenim brojem mobitela!");
             continue;
         }
+
         Console.WriteLine("1 - Ispis svih poziva s kontaktaom");
         Console.WriteLine("2 - Pozivanje kontakta");
         Console.WriteLine("0 - Povratak na glavni izbornik");
@@ -83,7 +229,25 @@ void ManageContact()
                 PrintCalls(contact);
                 break;
             case "2":
-                contact.PlaceCall();
+                var status = contact.PlaceCall();
+                if (status.Item2 == null)
+                {
+                    Console.WriteLine("Kontakt je blokiran!");
+                }
+                else if (status.Item1 == 0)
+                {
+                    Console.WriteLine(status.Item1);
+                    Console.WriteLine("Poziv nije odgovoren!");
+                    phoneBook[contact].Add(status.Item2);
+                }
+                else
+                {
+                    Console.WriteLine("Poziv je u tijeku!");
+                    Thread.Sleep(status.Item1 * 1000);
+                    Console.WriteLine($"Poziv je završen! Trajanje poziva: {status.Item1} sekundi");
+                    status.Item2.ConcludeCall();
+                    phoneBook[contact].Add(status.Item2);
+                }
                 break;
             case "0":
                 return;
@@ -96,5 +260,16 @@ void ManageContact()
 
 void PrintCalls(Contact? contact = null)
 {
-    //TODO: Implement
+    if (contact == null)
+    {
+        foreach (var call in phoneBook.Values.SelectMany(calls => calls))
+            Console.WriteLine(
+                $"Vrijeme poziva: {call.TimeOfCall}, Status poziva: {call.Status}");
+    }
+    else
+    {
+        foreach (var call in phoneBook[contact])
+            Console.WriteLine(
+                $"Vrijeme poziva: {call.TimeOfCall}, Status poziva: {call.Status}");
+    }
 }
